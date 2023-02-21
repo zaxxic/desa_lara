4cfd299d000ec50c75c73e37d57a6a75487d600f
nepiiiy <nevi.yuanita01@gmail.com>
2023-02-21 14:02:17 +0700

    tambah reg

---
+++ resources/views/reg/login.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from brandio.io/envato/iofrm/html/job-app1.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 08 Feb 2023 07:22:43 GMT -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DESA.COM</title>
+    
+<link href="https://i.postimg.cc/wj308LVk/images.png" rel="shortcut icon" type="image/png">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/fontawesome-all.min.css">
+    <link rel="stylesheet" type="text/css" href="css/iofrm-style.css">
+    <link rel="stylesheet" type="text/css" href="css/iofrm-login.css">
+</head>
+<body>
+    <div class="form-body on-top-mobile">
+        <div class="website-logo" style="margin-left: 1050px;">
+                    <div><img src="https://i.postimg.cc/hGBKT07K/logo-removebg-preview.png" alt="" style="width:200px;"></div>
+        </div>
+        <div class="row">
+            <div class="img-holder" style="margin-left: 800px;">
+                <div class="bg"></div>
+                <div class="info-holder simple-info" >
+                    <div><img src="image-reg/graphic6.svg" alt=""></div>
+                    <div><h3>Login</h3></div>
+                    <div><p>Masukkan email dan password untuk login</p></div>
+                </div>
+            </div>
+            <div class="form-holder">
+                <div class="form-content">
+                    <div class="form-items">
+                        <div class="form-items" style="margin-left: 250px;">
+                            <h3>Login</h3>
+                        </div>
+                        <form>
+                            <div class="row">
+                                <div class="col-12">
+                                    <input type="text" class="form-control" placeholder="Email Desa" name="email">
+                                    <input type="text" class="form-control" placeholder="Password" name="password">
+                                </div>
+                            </div>
+                            <div class="row top-padding">
+                                <div class="col-12 col-sm-6">
+                                    <label for="chk1">Belum punya akun ? <a href="registrasi.html" style="text-decoration: none;">Daftar sekarang</a></label>
+                                </div>
+                                <div class="col-12 col-sm-6">
+                                    <div class="form-button text-right">
+                                        <button id="submit" type="submit" class="ibtn less-padding" style="background-color: #F26522; color: #ffffff"><a href="../admin-desa/dashboard.html" style="text-decoration: none; color: #ffffff">Login</a></button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+
+<!-- Mirrored from brandio.io/envato/iofrm/html/job-app1.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 08 Feb 2023 07:22:43 GMT -->
+</html>
---
+++ resources/views/reg/registrasi.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from brandio.io/envato/iofrm/html/job-app1.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 08 Feb 2023 07:22:43 GMT -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DESA.COM</title>
+    
+<link href="https://i.postimg.cc/wj308LVk/images.png" rel="shortcut icon" type="image/png">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/fontawesome-all.min.css">
+    <link rel="stylesheet" type="text/css" href="css/iofrm-style.css">
+    <link rel="stylesheet" type="text/css" href="css/iofrm-theme23.css">
+</head>
+<body>
+    <div class="form-body on-top-mobile">
+        <div class="website-logo">
+                    <div><img src="https://i.postimg.cc/hGBKT07K/logo-removebg-preview.png" alt="" style="width:200px;"></div>
+        </div>
+        <div class="row">
+            <div class="img-holder">
+                <div class="bg"></div>
+                <div class="info-holder simple-info">
+                    <div><img src="image-reg/graphic6.svg" alt=""></div>
+                    <div><h3>Registrasi</h3></div>
+                    <div><p>Masukkan data yang sesuai untuk mendaftarkan desa anda</p></div>
+                </div>
+            </div>
+            <div class="form-holder">
+                <div class="form-content">
+                    <div class="form-items">
+                        <form style="margin-top: 90px;">
+                            <div class="row">
+                                <div class="col-12">
+                                    <input type="text" class="form-control" placeholder="Nama Desa" name="username">
+                                    <input type="text" class="form-control" placeholder="Email Desa" name="email">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12">
+                                    <input type="text" class="form-control" placeholder="Password" name="password">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12 col-sm-3">
+                                    <select type="text" class="form-control" placeholder="Provinsi" name="provinsi">
+                                        <option selected>Provinsi</option>
+                                    </select>
+                                </div>
+                                <div class="col-12 col-sm-3">
+                                    <select type="text" class="form-control" placeholder="Kabupaten/Kota" name="kabupaten">
+                                        <option selected>Kabupaten/Kota</option>
+                                    </select>
+                                </div>
+                                <div class="col-12 col-sm-3">
+                                    <select type="text" class="form-control" placeholder="Kecamatan" name="kecamatan">
+                                        <option selected>Kecamatan</option>
+                                    </select>
+                                </div>
+                                <div class="col-12 col-sm-3">
+                                    <input type="text" class="form-control" placeholder="Kode Pos" name="kode_pos">
+                                </div>
+                            </div>
+                        
+                            <div class="row">
+                                <div class="col-12">
+                                    <input type="text" class="form-control" placeholder="Titik Koordinat" name="koordinat">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12 ">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" id="gambar" name="gambar">
+                                        <label class="custom-file-label" for="validatedCustomFile">Unggah Persetujuan</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row top-padding">
+                                <div class="col-12 col-sm-6">
+                                    <label for="chk1">Sudah punya akun ? <a href="login.html" style="text-decoration: none;">Kembali ke login</a></label>
+                                </div>
+                                <div class="col-12 col-sm-6">
+                                    <div class="form-button text-right">
+                                        <button id="submit" type="submit" class="ibtn less-padding" style="background-color: #F26522; "><a href="login.html" style="text-decoration: none;color: #ffffff">Daftar</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+
+<!-- Mirrored from brandio.io/envato/iofrm/html/job-app1.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 08 Feb 2023 07:22:43 GMT -->
+</html>
