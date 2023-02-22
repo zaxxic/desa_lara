cdb894980ae2f7ce96a71363e275e2cee0c5d85a
Dennisse001 <88120057+Dennisse001@users.noreply.github.com>
2023-02-22 11:23:41 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,13 @@ Route::middleware(['auth:sanctum','verified','cekrole'])->group(function(){
 Route::get('/about',[AboutController::class, 'about'])->name('about');
 Route::get('/contact',[ContactController::class, 'contact'])->name('contact');
 Route::get('/dashweb',[DashwebController::class, 'dashweb'])->name('dashweb');
+
+
+
+
+// admin desa
+Route::get('/dashboard',[DashboardController::class,'dashboard'])->name('dashboard');
+Route::get('/profile-desa',[ProfilDesaController::class,'profiledesa'])->name('profile-desa');
+Route::get('/struktur-karang',[StrukturKarangController::class,'karang'])->name('struktur-karang');
+Route::get('/peraturan',[PeraturanController::class,'peraturan'])->name('peraturan');
+Route::get('/struktur_desa',[StrukturDesaController::class,'strukturdesa'])->name('struktur_desa');
