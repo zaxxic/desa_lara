a4aaaae80f97df68e0becb42b36916a8404b94a2
Dennisse001 <88120057+Dennisse001@users.noreply.github.com>
2023-02-22 08:01:32 +0700

    adw

---
+++ database/seeders/LocationSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\location;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+
+class LocationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        location::create([
+            'provinsi'  => 'Jatim',
+            'kabupaten' => 'Malang',
+            'kecamatan' => 'Singosari',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ]);
+    }
+}
